283da9f722a19ac6c8b234a6602d63a9924f4d5b
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2024-04-19 00:31:20 +0400

    refactor constants to external file

---
+++ src/Mscc.GenerativeAI/Constants/Constants.cs
@@ -0,0 +1,8 @@
+namespace Mscc.GenerativeAI
+{
+    internal struct Constants
+    {
+        internal const uint ChunkSize = 8388608;
+        internal const long MaxUploadFileSize = 2147483648;
+    }
+}
