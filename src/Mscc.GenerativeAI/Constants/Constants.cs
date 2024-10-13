c4968aad510a9315972126f2ff8dea0a05db1b4c
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2024-10-13 08:32:54 +0400

    enhance returned error information #33

--- src/Mscc.GenerativeAI/Constants/Constants.cs
+++ src/Mscc.GenerativeAI/Constants/Constants.cs
@@ -5,5 +5,7 @@ namespace Mscc.GenerativeAI
         internal const uint ChunkSize = 8388608;
         internal const long MaxUploadFileSize = 2147483648;
         internal const string MediaType = "application/json";
+        internal const string RequestFailed = "Request failed";
+        internal const string RequestFailedWithStatusCode = "Request failed with Status Code: ";
     }
 }
