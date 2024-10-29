f5b38aa1730671a67ac77d251149b43521cdc930
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2024-10-29 13:23:39 +0400

    add Logprobs handling

--- src/Mscc.GenerativeAI/Types/Generative/Candidate.cs
+++ src/Mscc.GenerativeAI/Types/Generative/Candidate.cs
@@ -12,46 +12,65 @@ namespace Mscc.GenerativeAI
         /// Output only. Content parts of the candidate.
         /// </summary>
         public ContentResponse? Content { get; set; }
+
         /// <summary>
         /// Output only. The reason why the model stopped generating tokens. 
         /// If empty, the model has not stopped generating the tokens.
         /// </summary>
         //public FinishReason FinishReason { get; internal set; }
         public FinishReason? FinishReason { get; set; }
+
         /// <summary>
         /// Output only. Describes the reason the mode stopped generating tokens 
         /// in more detail. This is only filled when `finish_reason` is set.
         /// </summary>
         public string? FinishMessage { get; set; }
+
         /// <summary>
         /// Output only. Index of the candidate.
         /// </summary>
         public int? Index { get; set; }
+
         /// <summary>
         /// Output only. List of ratings for the safety of a response candidate. 
         /// There is at most one rating per category.
         /// </summary>
         public List<SafetyRating>? SafetyRatings { get; set; }
+
         /// <summary>
         /// Output only. Source attribution of the generated content.
         /// </summary>
         public CitationMetadata? CitationMetadata { get; set; }
+
         /// <summary>
         /// 
         /// </summary>
         public FunctionCall? FunctionCall { get; set; }
+
         /// <summary>
         /// 
         /// </summary>
-        public GroundingMetadata? GroundingMetadata {  get; set; }
+        public GroundingMetadata? GroundingMetadata { get; set; }
+
         /// <summary>
         /// Output only. Token count for this candidate.
         /// </summary>
         public int? TokenCount { get; set; }
+
         /// <summary>
         /// Output only. Attribution information for sources that contributed to a grounded answer.
         /// This field is populated for GenerateAnswer calls.
         /// </summary>
         public List<GroundingAttribution>? GroundingAttributions { get; set; }
+
+        /// <summary>
+        /// Output only. 
+        /// </summary>
+        public float? AvgLogprobs { get; set; }
+
+        /// <summary>
+        /// Output only. Log-likelihood scores for the response tokens and top tokens
+        /// </summary>
+        public LogprobsResult? LogprobsResult { get; set; }
     }
 }
---
+++ src/Mscc.GenerativeAI/Types/Generative/LogprobsResult.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Mscc.GenerativeAI
+{
+    /// <summary>
+    /// Logprobs Result
+    /// </summary>
+    public class LogprobsResult
+    {
+        /// <summary>
+        /// Length = total number of decoding steps.
+        /// </summary>
+        public List<TopCandidates> TopCandidates { get; set; }
+        /// <summary>
+        /// Length = total number of decoding steps.
+        /// The chosen candidates may or may not be in topCandidates.
+        /// </summary>
+        public List<LogprobsCandidate> ChosenCanditates { get; set; }
+    }
+}
