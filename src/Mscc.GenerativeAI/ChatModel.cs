bdf8131193dc32d063081a68cb273876fc256850
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2024-11-19 23:36:21 +0400

    overwrite HTTP handling of API key

--- src/Mscc.GenerativeAI/ChatModel.cs
+++ src/Mscc.GenerativeAI/ChatModel.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 #endif
 using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace Mscc.GenerativeAI
@@ -12,6 +13,17 @@ namespace Mscc.GenerativeAI
     public class ChatModel : BaseModel
     {
         protected override string Version => ApiVersion.V1Beta;
+        
+        /// <inheritdoc cref="BaseModel"/>
+        public string? ApiKey
+        {
+            set
+            {
+                _apiKey = value;
+                if (value != null)
+                    Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+            }
+        }
         
         /// <summary>
         /// Initializes a new instance of the <see cref="ChatModel"/> class.
