0b0549664367ffa43ba712b04ef390913a49cc09
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2024-11-26 08:49:28 +0400

    add OpenAI sample app

---
+++ samples/OpenAI.Gemini/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
+
+var apiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
+var model = "gemini-1.5-flash";
+var prompt = "Explain to me how AI works.";
+
+OpenAIClientOptions options = new() { Endpoint = new Uri("https://generativelanguage.googleapis.com/v1beta/") };
+ChatClient client = new(model, new ApiKeyCredential(apiKey), options);
+ChatCompletion completion = client.CompleteChat(prompt);
+Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+
+var response = client.CompleteChat(prompt);
+Console.WriteLine($"[Raw]: {response.GetRawResponse()}");
