7ba44801ba165143189bd99644bb1a4faa063793
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2024-12-05 12:22:18 +0400

    list OpenAI models

--- samples/OpenAI.Gemini/Program.cs
+++ samples/OpenAI.Gemini/Program.cs
@@ -1,15 +1,23 @@
 ﻿using OpenAI;
 using OpenAI.Chat;
+using OpenAI.Models;
 using System.ClientModel;
 
 var apiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
 var model = "gemini-1.5-flash";
 var prompt = "Explain to me how AI works.";
 
-OpenAIClientOptions options = new() { Endpoint = new Uri("https://generativelanguage.googleapis.com/v1beta/") };
+OpenAIClientOptions options = new() { Endpoint = new Uri("https://generativelanguage.googleapis.com/v1beta/openai/") };
 ChatClient client = new(model, new ApiKeyCredential(apiKey), options);
 ChatCompletion completion = client.CompleteChat(prompt);
 Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
 
 var response = client.CompleteChat(prompt);
-Console.WriteLine($"[Raw]: {response.GetRawResponse()}");
+Console.WriteLine($"[Raw]: {response.GetRawResponse().Content}");
+
+OpenAIModelClient modelClient = new(new ApiKeyCredential(apiKey), options);
+OpenAIModelCollection models = modelClient.GetModels();
+foreach (OpenAIModel item in models)
+{
+    Console.WriteLine($"{item.Id} ({item.CreatedAt}) by {item.OwnedBy}");
+}
