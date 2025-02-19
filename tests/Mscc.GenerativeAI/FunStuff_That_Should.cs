768ea770452ed50bb42edbcdfac521b58c1f163a
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-02-19 10:38:59 +0400

    add more test cases

--- tests/Mscc.GenerativeAI/FunStuff_That_Should.cs
+++ tests/Mscc.GenerativeAI/FunStuff_That_Should.cs
@@ -15,6 +15,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Mscc.GenerativeAI;
 using System.IO;
+using System.Runtime.InteropServices;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -25,23 +26,29 @@ namespace Test.Mscc.GenerativeAI
     {
         private readonly string _model = Model.Gemini20FlashThinkingExperimental;
 
-        [Fact]
-        public async Task Tell_A_Programming_Joke()
+        [Theory]
+        [InlineData("Tell me a programming joke")]
+        [InlineData("Tell me a dad joke")]
+        public async Task Tell_A_Joke(string prompt)
         {
             var googleAi = new GoogleAI(apiKey: fixture.ApiKey);
             var model = googleAi.GenerativeModel(model: _model);
             var response = await model.GenerateContent(
-                prompt: "Tell me a programming joke");
+                prompt: prompt);
             output.WriteLine(response.Text);
         }
 
-        [Fact]
-        public async Task Tell_A_Dad_Joke()
+        [Theory]
+        [InlineData("Tell me about De Morgan in Boolean Algebra")]
+        public async Task Tell_Something_About_Mathematics(string prompt)
         {
+            var systemInstruction = new Content(@"You are an expert mathematics professor. 
+Give you response some thought and explain certain prompts in more details than the usual answer.
+In case that you are providing mathematical formula and equations, then use LaTeX syntax with inline mode");
             var googleAi = new GoogleAI(apiKey: fixture.ApiKey);
-            var model = googleAi.GenerativeModel(model: _model);
-            var response = await model.GenerateContent(
-                prompt: "Tell me a dad joke");
+            var model = googleAi.GenerativeModel(model: _model, 
+                systemInstruction: systemInstruction);
+            var response = await model.GenerateContent(prompt: prompt);
             output.WriteLine(response.Text);
         }
     }
