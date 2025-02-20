9a18893163dd3f64201428650165d119bf6461be
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-02-20 20:06:52 +0400

    add comment regarding `application/rtf`

--- src/Mscc.GenerativeAI/GenerativeAIExtensions.cs
+++ src/Mscc.GenerativeAI/GenerativeAIExtensions.cs
@@ -671,7 +671,7 @@ namespace Mscc.GenerativeAI
                 case "roff": return "application/x-troff";
                 case "rpm": return "audio/x-pn-realaudio-plugin";
                 case "rqy": return "text/x-ms-rqy";
-                case "rtf": return "text/rtf";  // "application/rtf";
+                case "rtf": return "text/rtf";  // "application/rtf" // although correct, it is not accepted by Gemini API;
                 case "rtx": return "text/richtext";
                 case "ruleset": return "application/xml";
                 case "s": return "text/plain";
