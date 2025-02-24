a74429efac2b0a33f35a938e77fd11c6f6c6683f
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-02-24 17:28:03 +0400

    chore update tests

--- tests/Mscc.GenerativeAI/VertexAi_Gemini15Pro_Should.cs
+++ tests/Mscc.GenerativeAI/VertexAi_Gemini15Pro_Should.cs
@@ -128,7 +128,7 @@ namespace Test.Mscc.GenerativeAI
             // Assert
             response.Should().NotBeNull();
             response.Candidates.Should().NotBeNull().And.HaveCount(1);
-            response.Text.Should().Be("84");
+            response.Text.Should().Contain("84");
             output.WriteLine($"Result: {response?.Text}");
         }
 
