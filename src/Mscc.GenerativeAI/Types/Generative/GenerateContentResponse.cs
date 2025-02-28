5c2045f6ceaeee362c4b06268dd5c9341cf6390a
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-02-28 20:50:01 +0400

    add default constructor due to serialization

--- src/Mscc.GenerativeAI/Types/Generative/GenerateContentResponse.cs
+++ src/Mscc.GenerativeAI/Types/Generative/GenerateContentResponse.cs
@@ -59,6 +59,11 @@ namespace Mscc.GenerativeAI
         protected ILogger Logger { get; }
 
         /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public GenerateContentResponse() { }
+        
+        /// <summary>
         /// Base constructor to set the <see cref="ILogger"/> instance.
         /// </summary>
         /// <param name="logger">Optional. Logger instance used for logging</param>
