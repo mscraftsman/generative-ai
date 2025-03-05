fcc3f38a53d81d4c72542b1aea1372270fb7fd84
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-03-05 14:08:37 +0400

    add sample for image generation

--- samples/Python.Playground/gemini-openai.py
+++ samples/Python.Playground/gemini-openai.py
@@ -3,12 +3,26 @@ import base64
 from openai import OpenAI
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from PIL import Image
+from io import BytesIO
 
 load_dotenv()  # take environment variables from .env.
 client = OpenAI(
     api_key=os.environ['GOOGLE_API_KEY'],
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
+
+
+response = client.images.generate(
+    model="imagen-3.0-generate-002",
+    prompt="a portrait of a sheepadoodle wearing a cape",
+    response_format='b64_json',
+    n=1,
+)
+
+for image_data in response.data:
+  image = Image.open(BytesIO(base64.b64decode(image_data.b64_json)))
+  image.show()
 
 # Generate content
 response = client.chat.completions.create(
