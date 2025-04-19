7d344d4e8d7b6e73d09d22e6ba92a2be2f319823
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-04-19 20:19:10 +0400

    add Validated function calling mode

--- src/Mscc.GenerativeAI/Enums/FunctionCallingConfigMode.cs
+++ src/Mscc.GenerativeAI/Enums/FunctionCallingConfigMode.cs
@@ -18,18 +18,23 @@ namespace Mscc.GenerativeAI
         /// Default model behavior, model decides to predict either a function call
         /// or a natural language response.
         /// </summary>
-        Auto = 1,
+        Auto,
         /// <summary>
         /// Model is constrained to always predicting a function call only.
         /// If "allowed_function_names" are set, the predicted function call will be
         /// limited to any one of "allowed_function_names", else the predicted
         /// function call will be any one of the provided "function_declarations".
         /// </summary>
-        Any = 2,
+        Any,
         /// <summary>
         /// Model will not predict any function call. Model behavior is same as when
         /// not passing any function declarations.
         /// </summary>
-        None = 3,
+        None,
+        /// <summary>
+        /// Model decides to predict either a function call or a natural language response,
+        /// but will validate function calls with constrained decoding.
+        /// </summary>
+        Validated
     }
 }
