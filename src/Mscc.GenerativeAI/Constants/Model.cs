58c71a07c78e3a3fefebc80658e8537580cef5b1
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-05-23 10:38:14 +0400

    add model `gemini-2.5-flash-preview-05-20` add model `gemini-2.5-flash-preview-tts` add model `gemini-2.5-pro-preview-tts` add model `gemma-3n-e4b-it` add model `gemini-2.5-flash-preview-native-audio-dialog` add model `gemini-2.5-flash-exp-native-audio-thinking-dialog` add model `imagen-4.0-generate-preview-05-20` add model `imagen-4.0-ultra-generate-exp-05-20` add model `lyria-002`

--- src/Mscc.GenerativeAI/Constants/Model.cs
+++ src/Mscc.GenerativeAI/Constants/Model.cs
@@ -14,17 +14,21 @@ namespace Mscc.GenerativeAI
         // Gemini 1.0
         // Discontinuation date: April 9, 2025.
         public const string Gemini10Pro = Gemini10Pro002;
+
         // Discontinuation date: April 9, 2025.
         public const string Gemini10Pro001 = "gemini-1.0-pro-001";
+
         // Discontinuation date: April 9, 2025.
         public const string Gemini10Pro002 = "gemini-1.0-pro-002";
         public const string GeminiProVision = "gemini-pro-vision";
         public const string Gemini10ProVision = "gemini-1.0-pro-vision";
+
         // Discontinuation date: April 9, 2025.
         public const string Gemini10ProVision001 = "gemini-1.0-pro-vision-001";
         public const string GeminiProVisionLatest = "gemini-1.0-pro-vision-latest";
         public const string GeminiUltra = "gemini-ultra";
         public const string GeminiUltraLatest = "gemini-1.0-ultra-latest";
+
         // Gemini 1.5
         public const string GeminiPro = Gemini15Pro;
         public const string Gemini15Pro = "gemini-1.5-pro";
@@ -56,7 +60,7 @@ namespace Mscc.GenerativeAI
         public const string LearnLM = LearnLMExperimental;
         public const string LearnLM15 = LearnLMExperimental;
         public const string LearnLMExperimental = "learnlm-1.5-pro-experimental";
-        
+
         // Gemini 2.0
         public const string Gemini20Pro = Gemini20ProExperimental;
         public const string Gemini20ProExperimental = "gemini-2.0-pro-exp";
@@ -82,27 +86,34 @@ namespace Mscc.GenerativeAI
         public const string GeminiEmbedding = GeminiEmbeddingExperimental;
         public const string GeminiEmbeddingExperimental = "gemini-embedding-exp";
         public const string GeminiEmbeddingExperimental0307 = "gemini-embedding-exp-03-07";
-        
+
         // Gemini 2.5
         public const string Gemini25Flash = Gemini25FlashPreview;
-        public const string Gemini25FlashPreview = Gemini25FlashPreview0417;
+        public const string Gemini25FlashPreview = Gemini25FlashPreview0520;
         public const string Gemini25FlashPreview0417 = "gemini-2.5-flash-preview-04-17";
+        public const string Gemini25FlashPreview0520 = "gemini-2.5-flash-preview-05-20";
         public const string Gemini25FlashThinking = Gemini25FlashPreviewThinking;
         public const string Gemini25FlashPreviewThinking = Gemini25FlashPreview0417Thinking;
         public const string Gemini25FlashPreview0417Thinking = "gemini-2.5-flash-preview-04-17-thinking";
+        public const string Gemini25FlashPreviewTts = "gemini-2.5-flash-preview-tts";
+        public const string Gemini25FlashPreviewNativeAudio = "gemini-2.5-flash-preview-native-audio-dialog";
+        public const string Gemini25FlashPreviewNativeAudioThinking = "gemini-2.5-flash-exp-native-audio-thinking-dialog";
         public const string Gemini25Pro = Gemini25ProExperimental;
         public const string Gemini25ProExperimental = Gemini25ProExperimental0325;
         public const string Gemini25ProExperimental0325 = "gemini-2.5-pro-exp-03-25";
         public const string Gemini25ProPreview = Gemini25ProPreview0506;
         public const string Gemini25ProPreview0325 = "gemini-2.5-pro-preview-03-25";
         public const string Gemini25ProPreview0506 = "gemini-2.5-pro-preview-05-06";
-        
+        public const string Gemini25ProPreviewTts = "gemini-2.5-pro-preview-tts";
+
         // Gemma 3
         public const string Gemma3 = Gemma3_27B;
         public const string Gemma3_1B = "gemma-3-1b-it";
         public const string Gemma3_4B = "gemma-3-4b-it";
         public const string Gemma3_12B = "gemma-3-12b-it";
         public const string Gemma3_27B = "gemma-3-27b-it";
+        public const string Gemma3n = Gemma3n_E4B;
+        public const string Gemma3n_E4B = "gemma-3n-e4b-it";
 
         // PaLM 2 models
         public const string BisonText001 = "text-bison-001";
@@ -123,15 +134,16 @@ namespace Mscc.GenerativeAI
         public const string CodeBisonChat32k002 = "codechat-bison-32k-002";
         public const string CodeBisonChat32k = BisonChat32k002;
         public const string CodeGecko001 = "code-gecko-001";
+
         // Discontinuation date: April 9, 2025.
-        public const string CodeGecko002 = "code-gecko-002";    // Vertex: code-gecko@002
+        public const string CodeGecko002 = "code-gecko-002"; // Vertex: code-gecko@002
         public const string CodeGeckoLatest = "code-gecko@latest";
         public const string CodeGecko = "code-gecko";
         public const string GeckoEmbedding = "embedding-gecko-001";
         public const string Embedding001 = "embedding-001";
         public const string Embedding = Embedding001;
         public const string AttributedQuestionAnswering = "aqa";
-        
+
         // Text Embeddings on Vertex AI
         // Discontinuation date: Nov 18, 2025.
         public const string TextEmbedding004 = "text-embedding-004";
@@ -140,13 +152,18 @@ namespace Mscc.GenerativeAI
         public const string TextEmbeddingPreview0815 = "text-embedding-preview-0815";
         public const string TextEmbeddingPreview = TextEmbeddingPreview0815;
         public const string TextMultilingualEmbedding = "text-multilingual-embedding-002";
+
         // Discontinuation date: April 9, 2025.
         public const string GeckoTextEmbedding001 = "textembedding-gecko@001";
+
         // Discontinuation date: April 9, 2025.
         public const string GeckoTextEmbedding002 = "textembedding-gecko@002";
+
         // Discontinuation date: May 14, 2025.
         public const string GeckoTextEmbedding003 = "textembedding-gecko@003";
+
         public const string GeckoTextEmbedding = GeckoTextEmbedding003;
+
         // Discontinuation date: May 14, 2025.
         public const string GeckoTextMultilingualEmbedding001 = "textembedding-gecko-multilingual@001";
         public const string GeckoTextMultilingualEmbedding = GeckoTextMultilingualEmbedding001;
@@ -154,8 +171,12 @@ namespace Mscc.GenerativeAI
         // Multimodal Embeddings on Vertex AI
         public const string MultimodalEmbedding001 = "multimodalembedding@001";
         public const string MultimodalEmbedding = MultimodalEmbedding001;
-        
+
         // Models for Imagen on Vertex AI - image generation and editing
+        public const string Imagen4 = Imagen4Preview;
+        public const string Imagen4Preview = "imagen-4.0-generate-preview-05-20";
+        public const string Imagen4Ultra = Imagen4UltraExperimental;
+        public const string Imagen4UltraExperimental = "imagen-4.0-ultra-generate-exp-05-20";
         public const string Imagen3 = Imagen3Generate002;
         public const string Imagen3Generate001 = "imagen-3.0-generate-001";
         public const string Imagen3Generate002 = "imagen-3.0-generate-002";
@@ -163,17 +184,8 @@ namespace Mscc.GenerativeAI
         public const string Imagen3Fast = Imagen3GenerateFast001;
         public const string Imagen3GenerateFast = Imagen3GenerateFast001;
         public const string Imagen3GenerateFast001 = "imagen-3.0-fast-generate-001";
-        /// <summary>
-        /// Imagen 3 Generation is a Pre-GA. Allowlisting required.
-        /// </summary>
         public const string Imagen3Customization = "imagen-3.0-capability-001";
-        /// <summary>
-        /// Imagen 3 Generation is a Pre-GA. Allowlisting required.
-        /// </summary>
         public const string ImageGeneration3 = "imagen-3.0-generate-0611";
-        /// <summary>
-        /// Imagen 3 Generation is a Pre-GA. Allowlisting required.
-        /// </summary>
         public const string ImageGeneration3Fast = "imagen-3.0-fast-generate-0611";
         public const string ImageVerification = "image-verification-001";
         public const string ImageGeneration006 = "imagegeneration@006";
@@ -184,15 +196,17 @@ namespace Mscc.GenerativeAI
         public const string ImageGeneration = Imagen2;
         public const string ImageText001 = "imagetext@001";
         public const string ImageText = "imagetext";
-        
+
         // Veo
         public const string Veo = Veo2Generate001;
         public const string Veo2 = Veo2Generate001;
         public const string Veo2Generate = Veo2Generate001;
         public const string Veo2Generate001 = "veo-2.0-generate-001";
-        
+        //public const string Veo3 = "";
+
         // Lyria
         public const string Lyria = LyriaBase001;
         public const string LyriaBase001 = "lyria-base-001";
+        public const string Lyria2 = "lyria-002";
     }
 }
