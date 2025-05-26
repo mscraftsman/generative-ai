994530036b9aee1e0fa828ebb3b27b3f947bcfc7
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-05-26 11:11:35 +0400

    add image manipulation config types

---
+++ src/Mscc.GenerativeAI/Types/Configuration/StyleReferenceConfig.cs
@@ -0,0 +1,13 @@
+namespace Mscc.GenerativeAI
+{
+    /// <summary>
+    /// Configuration for a Style reference image.
+    /// </summary>
+    public class StyleReferenceConfig
+    {
+        /// <summary>
+        /// A text description of the style to use for the generated image.
+        /// </summary>
+        public string? StyleDescription { get; set; }
+    }
+}
