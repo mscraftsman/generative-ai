64fe441f2dd75b0d21e0070348a32a763716de0a
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-06-03 15:40:16 +0400

    add property `ResponseJsonSchema`

--- src/Mscc.GenerativeAI/Types/Configuration/GenerationConfig.cs
+++ src/Mscc.GenerativeAI/Types/Configuration/GenerationConfig.cs
@@ -121,5 +121,37 @@ namespace Mscc.GenerativeAI
         /// Optional. Config for thinking features. An error will be returned if this field is set for models that don't support thinking.
         /// </summary>
         public ThinkingConfig? ThinkingConfig { get; set; }
+        /// <summary>
+        /// Optional. Output schema of the generated response.
+        /// This is an alternative to `response_schema` that accepts [JSON Schema](https://json-schema.org/).
+        /// If set, `response_schema` must be omitted, but `response_mime_type` is required.
+        /// While the full JSON Schema may be sent, not all features are supported.
+        /// Specifically, only the following properties are supported:
+        /// - `$id`
+        /// - `$defs`
+        /// - `$ref`
+        /// - `$anchor`
+        /// - `type`
+        /// - `format`
+        /// - `title`
+        /// - `description`
+        /// - `enum` (for strings and numbers)
+        /// - `items`
+        /// - `prefixItems`
+        /// - `minItems`
+        /// - `maxItems`
+        /// - `minimum`
+        /// - `maximum`
+        /// - `anyOf`
+        /// - `oneOf` (interpreted the same as `anyOf`)
+        /// - `properties`
+        /// - `additionalProperties`
+        /// - `required`
+        /// The non-standard `propertyOrdering` property may also be set.
+        /// Cyclic references are unrolled to a limited degree and, as such, may only be used within non-required
+        /// properties. (Nullable properties are not sufficient.) If `$ref` is set on a sub-schema, no other properties,
+        /// except for than those starting as a `$`, may be set.
+        /// </summary>
+        public object? ResponseJsonSchema { get; set; }
     }
 }
