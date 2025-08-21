846d8774e263582e17619fc1bfcb0415d4ad8a0d
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-08-21 11:35:45 +0400

    sync with API discovery

---
+++ src/Mscc.GenerativeAI/Types/Generative/QueryCorpusRequest.cs
@@ -0,0 +1,27 @@
+#if NET472_OR_GREATER || NETSTANDARD2_0
+using System.Collections.Generic;
+#endif
+
+namespace Mscc.GenerativeAI
+{
+    /// <summary>
+    /// Request for querying a `Corpus`.
+    /// </summary>
+    public class QueryCorpusRequest
+    {
+        /// <summary>
+        /// Required. Query string to perform semantic search.
+        /// </summary>
+        public string Query { get; set; }
+        /// <summary>
+        /// Optional. Filter for `Chunk` and `Document` metadata.
+        /// Each `MetadataFilter` object should correspond to a unique key. Multiple `MetadataFilter` objects are joined by logical \"AND\"s. Example query at document level: (year >= 2020 OR year < 2010) AND (genre = drama OR genre = action) `MetadataFilter` object list: metadata_filters = [ {key = \"document.custom_metadata.year\" conditions = [{int_value = 2020, operation = GREATER_EQUAL}, {int_value = 2010, operation = LESS}]}, {key = \"document.custom_metadata.year\" conditions = [{int_value = 2020, operation = GREATER_EQUAL}, {int_value = 2010, operation = LESS}]}, {key = \"document.custom_metadata.genre\" conditions = [{string_value = \"drama\", operation = EQUAL}, {string_value = \"action\", operation = EQUAL}]}] Example query at chunk level for a numeric range of values: (year > 2015 AND year <= 2020) `MetadataFilter` object list: metadata_filters = [ {key = \"chunk.custom_metadata.year\" conditions = [{int_value = 2015, operation = GREATER}]}, {key = \"chunk.custom_metadata.year\" conditions = [{int_value = 2020, operation = LESS_EQUAL}]}] Note: \"AND\"s for the same key are only supported for numeric values. String values only support \"OR\"s for the same key.
+        /// </summary>
+        public List<MetadataFilter>? MetadataFilters { get; set; }
+        /// <summary>
+        /// Optional. The maximum number of `Chunk`s to return.
+        /// The service may return fewer `Chunk`s. If unspecified, at most 10 `Chunk`s will be returned. The maximum specified result count is 100.
+        /// </summary>
+        public int? ResultsCount { get; set; }
+    }
+}
