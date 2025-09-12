833fc3975d0d77739586447ed29cc6379faed936
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-09-12 16:20:03 +0400

    add mapping to `Predictions`

--- src/Mscc.GenerativeAI/Types/ImageGeneration/EditImageResponse.cs
+++ src/Mscc.GenerativeAI/Types/ImageGeneration/EditImageResponse.cs
@@ -7,8 +7,12 @@ namespace Mscc.GenerativeAI
     /// <summary>
     /// Response for the request to edit an image.
     /// </summary>
-    public class EditImageResponse
+    public class EditImageResponse : ImageGenerationResponse
     {
+        /// <summary>
+        /// Output only. A list of the generated images.
+        /// </summary>
+        public List<Image> Images => Predictions;
         /// <summary>
         /// List of generated images.
         /// </summary>
