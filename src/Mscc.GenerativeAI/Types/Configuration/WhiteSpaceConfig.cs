09f20d86421a7b9400c8c27c4b59ed697a8c2eec
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-10-17 10:29:33 +0400

    add type `WhiteSpaceConfig`

---
+++ src/Mscc.GenerativeAI/Types/Configuration/WhiteSpaceConfig.cs
@@ -0,0 +1,25 @@
+namespace Mscc.GenerativeAI
+{
+    /// <summary>
+    /// Configuration for a white space chunking algorithm [white space delimited].
+    /// </summary>
+    public class WhiteSpaceConfig
+    {
+        /// <summary>
+        /// Maximum number of overlapping tokens between two adjacent chunks.
+        /// </summary>
+        public int? MaxOverlapTokens { get; set; }
+        /// <summary>
+        /// Maximum number of tokens per chunk.
+        /// </summary>
+        /// <remarks>
+        /// Maximum number of tokens per chunk. Tokens are defined as words for this chunking algorithm.
+        /// Note: we are defining tokens as words split by whitespace as opposed to the output of a tokenizer.
+        /// The context window of the latest gemini embedding model as of 2025-04-17 is currently 8192 tokens.
+        /// We assume that the average word is 5 characters. Therefore, we set the upper limit to 2**9, which is
+        /// 512 words, or 2560 tokens, assuming worst case a character per token.
+        /// This is a conservative estimate meant to prevent context window overflow.
+        /// </remarks>
+        public int? MaxTokensPerChunk { get; set; }
+    }
+}
