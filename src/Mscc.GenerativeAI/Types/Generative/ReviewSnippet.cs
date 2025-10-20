87ebf3b4b4ef1480adf5436f20823f2b009f24bc
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-10-20 14:30:48 +0400

    add properties of Google AI

--- src/Mscc.GenerativeAI/Types/Generative/ReviewSnippet.cs
+++ src/Mscc.GenerativeAI/Types/Generative/ReviewSnippet.cs
@@ -14,8 +14,24 @@ namespace Mscc.GenerativeAI
         /// </summary>
         public string? Uri { get; set; }
         /// <summary>
+        /// A link that corresponds to the user review on Google Maps.
+        /// </summary>
+        public string? GoogleMapsUri
+        {
+            get => Uri;
+            set => Uri = value;
+        }
+        /// <summary>
         /// The ID of the review snippet.
         /// </summary>
         public string? Id { get; set; }
+        /// <summary>
+        /// The ID of the review snippet.
+        /// </summary>
+        public string? ReviewId
+        {
+            get => Id;
+            set => Id = value;
+        }
     }
 }
