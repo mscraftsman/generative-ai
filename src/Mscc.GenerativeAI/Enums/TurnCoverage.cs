adc4e4ad3d00be6b3e356addae1b1efc0055768c
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-10-20 14:32:49 +0400

    add enum `TurnCoverage`

---
+++ src/Mscc.GenerativeAI/Enums/TurnCoverage.cs
@@ -0,0 +1,26 @@
+#if NET472_OR_GREATER || NETSTANDARD2_0
+using System.Text.Json.Serialization;
+#endif
+
+namespace Mscc.GenerativeAI
+{
+    /// <summary>
+    /// Defines which input is included in the user's turn.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter<TurnCoverage>))]
+    public enum TurnCoverage
+    {
+        /// <summary>
+        /// If unspecified, the default behavior is `TURN_INCLUDES_ONLY_ACTIVITY`.
+        /// </summary>
+        TurnCoverageUnspecified,
+        /// <summary>
+        /// The users turn only includes activity since the last turn, excluding inactivity (e.g. silence on the audio stream). This is the default behavior. 
+        /// </summary>
+        TurnIncludesOnlyActivity,
+        /// <summary>
+        /// The users turn includes all realtime input since the last turn, including inactivity (e.g. silence on the audio stream). 
+        /// </summary>
+        TurnIncludesAllInput
+    }
+}
