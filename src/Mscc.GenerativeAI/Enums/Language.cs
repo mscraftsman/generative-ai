5cbb7ffba08e02365d077da9efc95ffaa5fb0645
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-10-29 23:12:03 +0400

    extend enum `Language` with Bash

--- src/Mscc.GenerativeAI/Enums/Language.cs
+++ src/Mscc.GenerativeAI/Enums/Language.cs
@@ -7,6 +7,7 @@ namespace Mscc.GenerativeAI
     /// <summary>
     /// Defines the programming language for executable code.
     /// </summary>
+    /// <remarks>Python is the default language.</remarks>
     [JsonConverter(typeof(JsonStringEnumConverter<Language>))]
     public enum Language
     {
@@ -17,6 +18,10 @@ namespace Mscc.GenerativeAI
         /// <summary>
         /// Python >= 3.10, with numpy and simpy available.
         /// </summary>
-        Python
+        Python,
+        /// <summary>
+        /// Bash. Only available for Gemini 3.0 model or above.
+        /// </summary>
+        Bash
     }
 }
