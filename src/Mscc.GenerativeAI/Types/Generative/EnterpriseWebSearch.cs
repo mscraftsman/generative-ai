7b75d6130c0d551341b51a98aa368c890774a276
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-10-30 19:37:20 +0400

    add property `TimeRangeFilter` to `EnterpriseWebSearch`

--- src/Mscc.GenerativeAI/Types/Generative/EnterpriseWebSearch.cs
+++ src/Mscc.GenerativeAI/Types/Generative/EnterpriseWebSearch.cs
@@ -18,5 +18,10 @@ namespace Mscc.GenerativeAI
         /// Optional. Sites with confidence level chosen and above this value will be blocked from the search results.
         /// </summary>
         public PhishBlockThreshold? BlockingConfidence { get; set; }
+        /// <summary>
+        /// Optional. Filter search results to a specific time range. If customers set a start time,
+        /// they must set an end time (and vice versa). This field is not supported in Vertex AI.
+        /// </summary>  
+        public Interval? TimeRangeFilter { get; set; }
     }
 }
