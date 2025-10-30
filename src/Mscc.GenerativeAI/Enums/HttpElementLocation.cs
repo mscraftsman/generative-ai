4cb3e93937ae78e71a11625db97e9facd1fafce1
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-10-30 18:15:49 +0400

    add enum `HttpElementLocation`

---
+++ src/Mscc.GenerativeAI/Enums/HttpElementLocation.cs
@@ -0,0 +1,38 @@
+#if NET472_OR_GREATER || NETSTANDARD2_0
+using System.Text.Json.Serialization;
+#endif
+
+namespace Mscc.GenerativeAI
+{
+    /// <summary>
+    /// The location of the API key.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter<HttpElementLocation>))]
+    public enum HttpElementLocation
+    {
+        /// <summary>
+        /// Unspecified location.
+        /// </summary>
+        HttpInUnspecified,
+        /// <summary>
+        /// Element is in the HTTP request query.
+        /// </summary>
+        HttpInQuery,
+        /// <summary>
+        /// Element is in the HTTP request header.
+        /// </summary>
+        HttpInHeader,
+        /// <summary>
+        /// Element is in the HTTP request path.
+        /// </summary>
+        HttpInPath,
+        /// <summary>
+        /// Element is in the HTTP request body.
+        /// </summary>
+        HttpInBody,
+        /// <summary>
+        /// Element is in the HTTP request cookie.
+        /// </summary>
+        HttpInCookie
+    }
+}
