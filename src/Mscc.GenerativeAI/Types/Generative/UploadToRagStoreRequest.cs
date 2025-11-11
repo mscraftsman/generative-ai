c40e335c3e2ba8dc682b95151348cfc29c30fadf
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-11-11 10:43:55 +0400

    change `CustomMetadata` to a list

--- src/Mscc.GenerativeAI/Types/Generative/UploadToRagStoreRequest.cs
+++ src/Mscc.GenerativeAI/Types/Generative/UploadToRagStoreRequest.cs
@@ -1,3 +1,7 @@
+#if NET472_OR_GREATER || NETSTANDARD2_0
+using System.Collections.Generic;
+#endif
+
 namespace Mscc.GenerativeAI
 {
     /// <summary>
@@ -6,19 +10,21 @@ namespace Mscc.GenerativeAI
     public class UploadToRagStoreRequest
     {
         /// <summary>
-        /// Optional. Config for telling the service how to chunk the data. If not provided, the service will use default parameters.
+        /// Optional. Config for telling the service how to chunk the data.
+        /// If not provided, the service will use default parameters.
         /// </summary>
         public ChunkingConfig? ChunkingConfig { get; set; }
         /// <summary>
         /// Custom metadata to be associated with the data.
         /// </summary>
-        public CustomMetadata? CustomMetadata { get; set; }
+        public List<CustomMetadata>? CustomMetadata { get; set; }
         /// <summary>
         /// Optional. Display name of the created document.
         /// </summary>
         public string? DisplayName { get; set; }
         /// <summary>
-        /// Optional. MIME type of the data. If not provided, it will be inferred from the uploaded content.
+        /// Optional. MIME type of the data.
+        /// If not provided, it will be inferred from the uploaded content.
         /// </summary>
         public string? MimeType { get; set; }
     }
