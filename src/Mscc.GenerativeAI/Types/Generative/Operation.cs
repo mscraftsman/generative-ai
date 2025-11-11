8ca923ac0386d921319cf722dd939c0f56c0a7e8
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-11-11 10:45:10 +0400

    add common interface for different `Operations`

--- src/Mscc.GenerativeAI/Types/Generative/Operation.cs
+++ src/Mscc.GenerativeAI/Types/Generative/Operation.cs
@@ -26,11 +26,52 @@ namespace Mscc.GenerativeAI
         /// </remarks>
         public List<string>? Unreachable { get; set; }
     }
-    
+
+    public interface IOperation
+    {
+        /// <summary>
+        /// The server-assigned name, which is only unique within the same service that originally returns it.
+        /// If you use the default HTTP mapping, the `name` should be a resource name ending with `operations/{unique_id}`.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// If the value is `false`, it means the operation is still in progress.
+        /// If `true`, the operation is completed, and either `error` or `response` is available.
+        /// </summary>
+        bool Done { get; set; }
+
+        /// <summary>
+        /// The error result of the operation in case of failure or cancellation.
+        /// </summary>
+        Status? Error { get; set; }
+
+        /// <summary>
+        /// Service-specific metadata associated with the operation.
+        /// </summary>
+        /// <remarks>
+        /// It typically contains progress information and common metadata such as create time.
+        /// Some services might not provide such metadata. Any method that returns a long-running operation
+        /// should document the metadata type, if any.
+        /// </remarks>
+        object? Metadata { get; set; }
+
+        /// <summary>
+        /// The normal, successful response of the operation.
+        /// </summary>
+        /// <remarks>
+        /// If the original method returns no data on success, such as `Delete`, the response is `google.protobuf.Empty`.
+        /// If the original method is standard `Get`/`Create`/`Update`, the response should be the resource.
+        /// For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name.
+        /// For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
+        /// </remarks>
+        object? Response { get; set; }
+    }
+
     /// <summary>
     /// This resource represents a long-running operation that is the result of a network API call.
     /// </summary>
-    public class Operation
+    public class Operation : IOperation
     {
         /// <summary>
         /// The server-assigned name, which is only unique within the same service that originally returns it.
@@ -54,7 +95,7 @@ namespace Mscc.GenerativeAI
         /// Some services might not provide such metadata. Any method that returns a long-running operation
         /// should document the metadata type, if any.
         /// </remarks>
-        public object Metadata { get; set; }
+        public object? Metadata { get; set; }
         /// <summary>
         /// The normal, successful response of the operation.
         /// </summary>
@@ -64,7 +105,7 @@ namespace Mscc.GenerativeAI
         /// For other methods, the response should have the type `XxxResponse`, where `Xxx` is the original method name.
         /// For example, if the original method name is `TakeSnapshot()`, the inferred response type is `TakeSnapshotResponse`.
         /// </remarks>
-        public object Response { get; set; }
+        public object? Response { get; set; }
     }
 
     /// <summary>
