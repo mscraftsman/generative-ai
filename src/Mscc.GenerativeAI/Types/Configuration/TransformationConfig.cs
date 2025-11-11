7728740e9203d6d0815052bf9d01df80a121d5de
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-11-11 10:43:04 +0400

    chore: add XMLdocs

--- src/Mscc.GenerativeAI/Types/Configuration/TransformationConfig.cs
+++ src/Mscc.GenerativeAI/Types/Configuration/TransformationConfig.cs
@@ -2,6 +2,10 @@ namespace Mscc.GenerativeAI
 {
     public class TransformationConfig
     {
-        public ChunkingConfig ChunkingConfig { get; set; }
+        /// <summary>
+        /// Optional. Config for telling the service how to chunk the data.
+        /// If not provided, the service will use default parameters.
+        /// </summary>
+        public ChunkingConfig? ChunkingConfig { get; set; }
     }
 }
