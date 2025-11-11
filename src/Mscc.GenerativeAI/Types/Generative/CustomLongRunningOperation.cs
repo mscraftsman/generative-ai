35393cece06c0944f5ae555a31a64802265201ab
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-11-11 10:45:31 +0400

    add `CustomLongRunningOperation`

--- src/Mscc.GenerativeAI/Types/Generative/CustomLongRunningOperation.cs
+++ src/Mscc.GenerativeAI/Types/Generative/CustomLongRunningOperation.cs
@@ -4,8 +4,17 @@ namespace Mscc.GenerativeAI
     /// This is a copy of google.longrunning.Operation.
     /// We need to copy it because for interacting with scotty, we need to add a scotty specific field that can't be added in the top level Operation proto.
     /// </summary>
-    public class CustomLongRunningOperation : Operation
+    public class CustomLongRunningOperation : IOperation
     {
-        
+        /// <inheritdoc/>
+        public string Name { get; set; }
+        /// <inheritdoc/>
+        public bool Done { get; set; }
+        /// <inheritdoc/>
+        public Status? Error { get; set; }
+        /// <inheritdoc/>
+        public object? Metadata { get; set; }
+        /// <inheritdoc/>
+        public object? Response { get; set; }
     }
 }
