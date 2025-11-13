20798e1428a5d4800eecfe709169d9bc7b9b8a7d
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-11-13 08:23:52 +0400

    add properties for Vertex AI

--- src/Mscc.GenerativeAI/Types/Configuration/ImageConfig.cs
+++ src/Mscc.GenerativeAI/Types/Configuration/ImageConfig.cs
@@ -12,6 +12,19 @@ namespace Mscc.GenerativeAI
         /// Supported aspect ratios: 1:1, 2:3, 3:2, 3:4, 4:3, 9:16, 16:9, 21:9.
         /// If not specified, the model will choose a default aspect ratio based on any reference images provided.
         /// </remarks>
-        public ImageAspectRatio AspectRatio { get; set; }
+        public ImageAspectRatio? AspectRatio { get; set; }
+        /// <summary>
+        /// Optional. Specifies the size of generated images. Supported values are `1K`, `2K`, `4K`. If
+        /// not specified, the model will use default value `1K`
+        /// </summary>
+        public ImageSize? ImageSize { get; set; }
+        /// <summary>
+        /// MIME type of the generated image.
+        /// </summary>
+        public string? OutputMimeType { get; set; }
+        /// <summary>
+        /// Compression quality of the generated image (for ``image/jpeg`` only).
+        /// </summary>
+        public int? OutputCompressionQuality { get; set; }
     }
 }
