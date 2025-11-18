648bbea583aedcc8cf85f39502ab77d55592f800
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-11-18 09:21:07 +0400

    add property `ThinkingLevel`

--- src/Mscc.GenerativeAI/Types/Configuration/ThinkingConfig.cs
+++ src/Mscc.GenerativeAI/Types/Configuration/ThinkingConfig.cs
@@ -18,5 +18,9 @@ namespace Mscc.GenerativeAI
         /// Gemini 2.5 Pro: The thinking budget 1 is invalid. Please choose a value between 128 and 32768.
         /// </remarks>
         public int? ThinkingBudget { get; set; }
+        /// <summary>
+        /// Optional. The level of thoughts tokens that the model should generate.
+        /// </summary>
+        public ThinkingLevel? ThinkingLevel { get; set; }
     }
 }
