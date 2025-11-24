71afc7ae8dc63f8a0611ee8356d1b2eb85aab99b
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-11-24 21:01:02 +0400

    remove duplicate code covered by generation

--- src/Mscc.GenerativeAI/Types/Blob.cs
+++ src/Mscc.GenerativeAI/Types/Blob.cs
@@ -13,21 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+using System.Diagnostics;
 
 namespace Mscc.GenerativeAI.Types
 {
 	/// <summary>
 	/// Raw media bytes. Text should not be sent as raw bytes, use the 'text' field.
 	/// </summary>
-	public partial class Blob
+	[DebuggerDisplay("{MimeType}")]
+	public partial class Blob : IPart
 	{
-		/// <summary>
-		/// Raw bytes for media formats.
-		/// </summary>
-		public byte[]? Data { get; set; }
-		/// <summary>
-		/// The IANA standard MIME type of the source data. Examples: - image/png - image/jpeg If an unsupported MIME type is provided, an error will be returned. For a complete list of supported types, see [Supported file formats](https://ai.google.dev/gemini-api/docs/prompting_with_media#supported_file_formats).
-		/// </summary>
-		public string? MimeType { get; set; }
-    }
+	}
 }
