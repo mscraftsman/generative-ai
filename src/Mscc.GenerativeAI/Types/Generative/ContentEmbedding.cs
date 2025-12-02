17dc6dda71581a835efe6eda97ed42b895d4be32
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-12-02 21:18:23 +0400

    add property `Shape`

--- src/Mscc.GenerativeAI/Types/Generative/ContentEmbedding.cs
+++ src/Mscc.GenerativeAI/Types/Generative/ContentEmbedding.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 
 namespace Mscc.GenerativeAI
 {
-    /// <summary>
-    /// A list of floats representing an embedding.
-    /// Ref: https://ai.google.dev/api/rest/v1beta/ContentEmbedding
-    /// </summary>
-    public class ContentEmbedding
-    {
-        /// <summary>
-        /// The embedding values.
-        /// </summary>
-        public List<float> Values { get; set; }
-    }
+	/// <summary>
+	/// A list of floats representing an embedding.
+	/// Ref: https://ai.google.dev/api/rest/v1beta/ContentEmbedding
+	/// </summary>
+	public class ContentEmbedding
+	{
+		/// <summary>
+		/// The embedding values.
+		/// </summary>
+		public List<float>? Values { get; set; }
+		/// <summary>
+		/// This field stores the soft tokens tensor frame shape (e.g. [1, 1, 256, 2048]).
+		/// </summary>
+		public List<int>? Shape { get; set; }
+	}
 }
