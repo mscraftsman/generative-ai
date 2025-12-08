7cf76338d690d8d3f959e5302023654d29c7ff94
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-12-08 16:03:42 +0400

    add sample using Microsoft Agent Framework

---
+++ samples/Microsoft.AgentFramework/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.Agents.AI;
+using Mscc.GenerativeAI.Microsoft;
+
+const string JokerInstructions = "You are good at telling jokes.";
+const string JokerName = "JokerAgent";
+
+string apiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY") ?? throw new InvalidOperationException("Please set the GOOGLE_API_KEY environment variable.");
+string model = Environment.GetEnvironmentVariable("GOOGLE_GENAI_MODEL") ?? "gemini-2.5-flash";
+
+// Using a community driven Mscc.GenerativeAI.Microsoft package
+
+ChatClientAgent agentCommunity = new(
+	new GeminiChatClient(apiKey, model),
+	name: JokerName,
+	instructions: JokerInstructions);
+
+AgentRunResponse response = await agentCommunity.RunAsync("Tell me a joke about a pirate.");
+Console.WriteLine($"Community client based agent response:\n{response}");
