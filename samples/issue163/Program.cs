41e940f47c58f676afdbeaaac6d351d13995f251
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-12-23 02:01:11 +0400

    add sample for issue #163

---
+++ samples/issue163/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.AI;
+using Mscc.GenerativeAI.Microsoft;
+using Mscc.GenerativeAI.Types;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
+
+var apiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
+
+// var model = "gemini-2.5-flash";
+var model = Model.Gemini3Pro;
+// var model = "gemini-3-flash-preview";
+var nativeClient = new GeminiChatClient(apiKey, model: model, logger: null);
+
+var chatClient = nativeClient
+	.AsBuilder()
+	.UseFunctionInvocation(loggerFactory: null, configure: (functionInvokingClient) =>
+	{
+		functionInvokingClient.MaximumIterationsPerRequest = 999;
+		functionInvokingClient.IncludeDetailedErrors = true;
+	})
+	.Build();
+
+var userMessage = new ChatMessage(ChatRole.User, "What is 2 + 2?");
+
+Console.WriteLine($"Q : {userMessage.Text}");
+
+var output = await chatClient.GetResponseAsync<int>(userMessage);
+
+Console.WriteLine($"A : {output.Result}");
+
+
+
+
+public static class Extensions
+{
+	public static ChatClientBuilder AsBuilder(this IChatClient client) => new ChatClientBuilder(client);
+}
