a2f29c7a99972095f083ab5111f8005cd6f90b6c
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2026-01-19 14:52:50 +0400

    upgrade file size limits

--- src/Mscc.GenerativeAI/Constants/Constants.cs
+++ src/Mscc.GenerativeAI/Constants/Constants.cs
@@ -20,6 +20,9 @@ namespace Mscc.GenerativeAI.Types
     {
         internal const uint ChunkSize = 8388608;
 	    internal const int EmbeddingDimensions = 768;
+	    internal const long MaxInlineDataFileSize = 104857600;
+	    internal const long MaxExternalUrlFileSize = 104857600;
+	    internal const long MaxRegisterFileSize = 2147483648;
         internal const long MaxUploadFileSize = 2147483648;
         internal const long MaxUploadFileSizeRagStore = 104857600;
         internal const long MaxUploadFileSizeFileSearchStore = 104857600;
